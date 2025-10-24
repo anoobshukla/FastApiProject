095054792afd5ab43ba50e757177683e384b6d19
Anoop Shukla <108170152+anoobshukla@users.noreply.github.com>
2025-10-24 23:43:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn 
+
+app = FastAPI()
+
+@app.get("/hello")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
